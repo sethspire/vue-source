fb4059f0470aaa49d9760478a773fe44dd39fdef
sethspire <sspire2@eagles.bridgewater.edu>
2023-03-15 11:54:18 -0400

    app 11

---
+++ app11/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import Card from './components/Card.vue'
+
+  import {ref, provide} from 'vue'
+
+  let maxNum = ref(5)
+
+  provide('maxNum', maxNum)
+</script>
+
+<template>
+  <h3>Primes and Composites</h3>
+  <p>The user can select how many numbers they want to display. That value is set in the parent (App.vue). This value is then shared with provide and each card element injects that value. Each card is also passed it's individual number via props from its parent card (just 1 more than that of the parent). If the number associated with said card is less than the value of numbers to show (from the injection), then another card is created. Also, background color is set so that all prime numbers are blue.</p>
+
+  <br>
+
+  <form>
+    <label for="maxNum">How many numbers (1-100) to display? <i>{{ maxNum }}</i></label>
+    <br>
+    <input type="range" id="maxNum" name="maxNum" min="0" max="100" class="form-control" v-model="maxNum">
+  </form>
+
+  <div>
+    <Card :num=1 />
+  </div>
+</template>
+
+<style scoped>
+  form, label, input, h3, p {
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+</style>
---
+++ app11/src/components/Card.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { inject} from 'vue'
+
+  const props = defineProps(['num'])
+
+  const maxNum = inject('maxNum')
+
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+</script>
+
+<template>
+  <div v-if="primes.includes(props.num)" class="container prime">
+    <span>{{ props.num }}</span>
+    <Card v-if="props.num < maxNum" :num=props.num+1 />
+  </div>
+  <div v-else class="container composite">
+    <span>{{ props.num }}</span>
+    <Card v-if="props.num < maxNum" :num=props.num+1 />
+  </div>
+</template>
+
+<style >
+  .container {
+    margin: 0 3px 1px 3px;
+    text-align: center;
+    border: 1px solid black;
+    border-radius: 8px;
+  } 
+  .prime {
+    background-color: blue;
+    color: white;
+  }
+  .composite {
+    background-color: white;
+    color: black;
+  }
+</style>
